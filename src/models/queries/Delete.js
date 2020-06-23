9ae55a04a99726297856a9692a1a6fc597fd6adc
Carcair <delic.emir90@gmail.com>
2020-06-23 14:32:24 +0200

    Deleted redundant files. Necessary code inserted in /src/models/Queries.js and their corresponding route files.

--- src/models/queries/Delete.js
+++
@@ -1,7 +0,0 @@
-class Delete {
-  constructor(table_name, filter_name, filter_value) {
-    this.sql = `DELETE FROM ${table_name} WHERE ${filter_name}='${filter_value}'`;
-  }
-}
-
-module.exports = Delete;
