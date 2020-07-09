43255fb9f5023e8023998ad40838ee58bbec495b
Carcair <delic.emir90@gmail.com>
2020-07-09 14:31:43 +0200

    Created sequelize model for user table

---
+++ src/models/Users.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const db = require('../methods/dbConnect');
+
+/**
+ * Model for user table
+ */
+const Users = db.define(
+  'user',
+  {
+    username: { type: Sequelize.STRING(255) },
+    pass: { type: Sequelize.STRING(255) },
+  },
+  { timestamps: false }
+);
+
+module.exports = Users;
