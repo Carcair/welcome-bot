04a95b4cf49f9b7c7890fb77e08642aec7a23aaf
Carcair <delic.emir90@gmail.com>
2020-07-09 14:30:56 +0200

    Helper methods for getting token out of header and it's verification

--- src/methods/helper.js
+++ src/methods/helper.js
@@ -1,3 +1,15 @@
+/**
+ * Load dependencies
+ */
+const jwt = require('jsonwebtoken');
+const logger = require('../config/logger');
+require('dotenv').config();
+
+/**
+ * Load necessary Sequelize models
+ */
+const Users = require('../models/Users');
+
 const helpers = {
   /**
    * Encode input
@@ -23,6 +35,58 @@ const helpers = {
       repeat_range: decodeURIComponent(self.repeat_range),
     };
     return temp;
+  },
+
+  /**
+   * Token format / Authorization: Bearer <access_token>
+   */
+
+  /**
+   * Callback fn for getting token out of the header
+   */
+  getToken(req, res, next) {
+    // get authorization header value
+    const bearerHeader = req.headers.authorization;
+    // Check if header is undefined
+    if (typeof bearerHeader !== 'undefined') {
+      // Take token out of header
+      const bearer = bearerHeader.split(' ');
+      const token = bearer[1];
+      req.token = token;
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+  },
+
+  /**
+   * Callback fn for token verification
+   */
+  verifyToken(req, res, next) {
+    jwt.verify(req.token, process.env.SK, (err, data) => {
+      if (err) {
+        logger.logDeniedAccess();
+        res.sendStatus(403);
+      } else {
+        Users.findAll({
+          where: {
+            id: data.results[0].id,
+          },
+        })
+          .then((result) => {
+            if (JSON.stringify(result) === '[]') {
+              logger.logAccessExpired();
+              res.status(406).end('false');
+            } else {
+              next();
+            }
+          })
+          .catch((err) => {
+            logger.logSQLError(err);
+            res.status(406).end(err.parent.sqlMessage);
+          });
+      }
+    });
   },
 };
 
