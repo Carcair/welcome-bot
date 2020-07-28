292c90bd1f09ba40a9015d5257648937d73b8faa
Carcair <delic.emir90@gmail.com>
2020-07-28 13:35:11 +0200

    Uniformed error responses. Added regular cron-task messages.

--- src/handlers/botHandler.js
+++ src/handlers/botHandler.js
@@ -2,42 +2,71 @@
  * Load dependencies
  */
 const SlackBot = require('slackbots');
+const CronJob = require('cron').CronJob;
 const { getMessage, getTriggers } = require('../methods/helper');
+const logger = require('../config/logger');
 
 /**
  * Load secret variables
  */
 const { botConfig } = require('../../config');
 
+/**
+ * Initialize Slack Bot
+ */
 const bot = new SlackBot(botConfig);
 
-const CronJob = require('cron').CronJob;
 /**
  * Test slackbot connection
  * general chat can be changed to whatever chat bot joins
  * then we can use it as reusable
  */
 bot.on('start', () => {
-  // var params = {};
-
   // Post a message to general channel
   bot.postMessageToChannel(
     'slackbot-test',
     'What does Marsellus Wallace look like?!'
   );
+
+  // Regular morning greeting at 8:30 from monday to friday
+  const goodMorning = new CronJob(
+    '30 8 * * 1-5',
+    () => {
+      bot.postMessageToChannel('general', 'Good Morning Folks! :wave:');
+    },
+    null,
+    true
+  );
+
+  /**
+   * Testing cron task which sends message every minute
+   * Presentation only
+   */
+  // const test = new CronJob(
+  //   '* * * * *',
+  //   () => {
+  //     bot.postMessageToChannel(
+  //       'slackbot-test',
+  //       'Testing message sent every minute. :punch:'
+  //     );
+  //   },
+  //   null,
+  //   true
+  // );
 });
 
 /**
  * Error handler
  */
-bot.on('error', (err) => console.log(err));
+bot.on('error', (err) => logger.logBotError(err));
 
 /**
  * Message handler
  */
 bot.on('message', (data) => {
-  // Break if event sent something unauthorized
+  // Continue only if user sent a MESSAGE directed at BOT
   if (data.type === 'message' && data.username !== 'Welcome Bot') {
+    console.log(data);
     /**
      * Check input for some defaults
      */
@@ -52,25 +81,44 @@ bot.on('message', (data) => {
           temp_array = JSON.parse(temp);
           temp_array.forEach((obj) => {
             let temp_output = `\`\`\`Command: ${obj.trigger_word}, posts on channel: ${obj.channel}, active: ${obj.active}\`\`\``;
+            // bot.postMessageToChannel(data.channel, temp_output);
             bot.postMessageToChannel('slackbot-test', temp_output);
           });
         })
         .catch((err) => {
-          bot.postMessageToChannel('slackbot-test', err);
+          logger.logSQLError(err);
+          bot.postMessageToChannel(
+            'slackbot-test',
+            'Can not connect to database.'
+          );
         });
       return;
     } else {
+      /**
+       * Sends back a message linked with trigger_word sent from Slack
+       */
       let temp = data.text.split(' ');
 
       getMessage(temp[1])
         .then((message) => {
+          /**
+           * Testing message post
+           */
+          // bot.postMessageToChannel(
+          //   'slackbot-test',
+          //   decodeURIComponent(message.text)
+          // );
+
+          /**
+           * Production message post
+           */
           bot.postMessageToChannel(
-            'slackbot-test',
+            message.channel,
             decodeURIComponent(message.text)
           );
-          // bot.postMessageToChannel(message.channel, message.text);
         })
         .catch((err) => {
+          logger.logSQLError(err);
           bot.postMessageToChannel(
             'slackbot-test',
             'Can not connect to database.'
@@ -80,14 +128,5 @@ bot.on('message', (data) => {
     }
   }
 });
-
-const job = new CronJob(
-  '22 13 * * *',
-  () => {
-    console.log('Testing');
-  },
-  null,
-  true
-);
 
 module.exports = bot;
