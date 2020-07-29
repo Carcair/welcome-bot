a4e31188a885314cdc4c9ce3624c35c20896981b
Carcair <delic.emir90@gmail.com>
2020-07-29 11:04:37 +0200

    Finished bot responses.

--- src/handlers/botHandler.js
+++ src/handlers/botHandler.js
@@ -21,39 +21,19 @@ const bot = new SlackBot(botConfig);
  * general chat can be changed to whatever chat bot joins
  * then we can use it as reusable
  */
-bot.on('start', () => {
-  // Post a message to general channel
-  bot.postMessageToChannel(
-    'slackbot-test',
-    'What does Marsellus Wallace look like?!'
-  );
-
-  // Regular morning greeting at 8:30 from monday to friday
-  const goodMorning = new CronJob(
-    '30 8 * * 1-5',
-    () => {
-      bot.postMessageToChannel('general', 'Good Morning Folks! :wave:');
-    },
-    null,
-    true
-  );
-
-  /**
-   * Testing cron task which sends message every minute
-   * Presentation only
-   */
-  // const test = new CronJob(
-  //   '* * * * *',
-  //   () => {
-  //     bot.postMessageToChannel(
-  //       'slackbot-test',
-  //       'Testing message sent every minute. :punch:'
-  //     );
-  //   },
-  //   null,
-  //   true
-  // );
-});
+// bot.on('start', () => {
+//   const test = new CronJob(
+//     '* * * * *',
+//     () => {
+//       bot.postMessageToChannel(
+//         'slackbot-test',
+//         'Testing message sent every minute. :punch:'
+//       );
+//     },
+//     null,
+//     true
+//   );
+// });
 
 /**
  * Error handler
@@ -66,6 +46,12 @@ bot.on('error', (err) => logger.logBotError(err));
 bot.on('message', (data) => {
   // Continue only if user sent a MESSAGE directed at BOT
   if (data.type === 'message' && data.username !== 'Welcome Bot') {
+    /**
+     * Username is contained in metadata only if the sender is Bot
+     * In other cases metadata contains only user ID's and channel ID's
+     * Necessary comparisons to get username and channel names
+     */
+
     /**
      * Check input for some defaults
      */
@@ -80,8 +66,24 @@ bot.on('message', (data) => {
           temp_array = JSON.parse(temp);
           temp_array.forEach((obj) => {
             let temp_output = `\`\`\`Command: ${obj.trigger_word}, posts on channel: ${obj.channel}, active: ${obj.active}\`\`\``;
-            // bot.postMessageToChannel(data.channel, temp_output);
-            bot.postMessageToChannel('slackbot-test', temp_output);
+
+            /**
+             * Comparing channel id where message to bot was sent
+             * with all channels
+             * then sending response in apropriate channel
+             */
+            bot
+              .getChannels()
+              .then((results) => {
+                results.channels.forEach((obj) => {
+                  if (data.channel === obj.id) {
+                    bot.postMessageToChannel(obj.name, temp_output);
+                  }
+                });
+              })
+              .catch((err) => {
+                logger.logBotError(err);
+              });
           });
         })
         .catch((err) => {
@@ -92,6 +94,56 @@ bot.on('message', (data) => {
           );
         });
       return;
+    } else if (data.text.includes(' robot-about')) {
+      /**
+       * Get channel and its users
+       */
+
+      // Get all channels
+      bot.channels.forEach((channelObj) => {
+        /**
+         * Comparing all channel ID's with channel ID where message was sent
+         */
+
+        if (channelObj.id === data.channel) {
+          // Ourput
+          let tempOutput = `Channel \`\`\`${channelObj.name}\`\`\` | Members: `;
+
+          // Get all users from workspace
+          bot.users.forEach((userObj) => {
+            // Undefined users no longer are part of workspace
+            // we need only users which belong to the channel we are requesting info from
+            if (
+              userObj.real_name !== undefined &&
+              channelObj.members.includes(userObj.id)
+            ) {
+              tempOutput += ` \`\`\` ${userObj.real_name} \`\`\` `;
+            }
+          });
+          bot.postMessageToChannel(channelObj.name, tempOutput);
+        }
+      });
+
+      /**
+       * Messages defined by application description
+       * but don't have detailed description of their purpose
+       */
+    } else if (data.text.includes(' robot-benefits')) {
+      /**
+       * General info about company benefits
+       */
+    } else if (data.text.includes(' robot-work')) {
+      /**
+       * Shows about flexible work hours
+       */
+    } else if (data.text.includes(' robot-docs')) {
+      /**
+       * Show link to documentation related to channel or general documentation
+       */
+    } else if (data.text.includes(' robot-community')) {
+      /**
+       * Show link about the Google+ community
+       */
     } else {
       /**
        * Sends back a message linked with trigger_word sent from Slack
@@ -101,20 +153,35 @@ bot.on('message', (data) => {
       getMessage(temp[1])
         .then((message) => {
           /**
-           * Testing message post
+           * If channel is defined as private
+           * Message must be sent to user in PM
            */
-          // bot.postMessageToChannel(
-          //   'slackbot-test',
-          //   decodeURIComponent(message.text)
-          // );
-
-          /**
-           * Production message post
-           */
-          bot.postMessageToChannel(
-            message.channel,
-            decodeURIComponent(message.text)
-          );
+          if (message.channel !== 'private') {
+            // Message sent to channel
+            bot.postMessageToChannel(
+              message.channel.toLowerCase(),
+              decodeURIComponent(message.text)
+            );
+          } else {
+            /**
+             * Comparing user id from metadata with all users id
+             * and getting username from that comparison
+             */
+            bot
+              .getUsers()
+              .then((results) => {
+                results.members.forEach((obj) => {
+                  if (obj.real_name !== undefined) {
+                    if (data.user === obj.id) {
+                      bot.postMessageToUser(obj.name, temp_output);
+                    }
+                  }
+                });
+              })
+              .catch((err) => {
+                logger.logBotError(err);
+              });
+          }
         })
         .catch((err) => {
           logger.logSQLError(err);
