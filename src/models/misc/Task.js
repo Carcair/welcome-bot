e13796df1458088628038c164d8453c2b43f5bf6
Carcair <delic.emir90@gmail.com>
2020-08-24 12:27:23 +0200

    Docker Fix

--- src/models/misc/Task.js
+++ src/models/misc/Task.js
@@ -126,7 +126,7 @@ class Task {
 
     this.job = new CronJob(
       // '* * * * *', // Cron task for every min, for tests
-      `20 12 ${this.initDay()} ${this.initMonth()} *`,
+      `30 12 ${this.initDay()} ${this.initMonth()} *`,
       () => {
         // On tick
         const self = this;
@@ -157,7 +157,7 @@ class Task {
           )
             .then(() => {
               // Set new crontime for this task
-              const tempString = `20 12 ${tempArray[0]} ${tempArray[1]} *`;
+              const tempString = `30 12 ${tempArray[0]} ${tempArray[1]} *`;
               self.job.setTime(new CronTime(tempString));
               self.sendMessage();
             })
