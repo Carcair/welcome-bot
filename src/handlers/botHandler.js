4461edfe7abce73f307665d0711e1fabc89fdb7e
Carcair <delic.emir90@gmail.com>
2020-09-01 08:34:58 +0200

    Bot lifecycle maintain

--- src/handlers/botHandler.js
+++ src/handlers/botHandler.js
@@ -28,6 +28,7 @@ const startBot = () => {
   let date = new Date();
   bot = new SlackBot(botConfig);
   bot.on('open', () => console.log('Websocket connection is open. || ' + date));
+  bot.postMessageToChannel('slackbot-test-2', 'Maintaining Bot lifecycle.');
 };
 startBot();
 
